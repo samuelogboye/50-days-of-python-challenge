8169fead7df31151cb16a65d92c6df3f005d6262
Samuel <ogboyesam@gmail.com>
2023-07-15 16:04:09 -0700

    Day17 updated

--- Day17/sort.py
+++ Day17/sort.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
-
-def sort_lenght(a):
-    for i in a:
-        print(max(i))
+def sort_length(strings):
+    for i in range(len(strings)):
+        min_index = i
+        for j in range(i + 1, len(strings)):
+            if len(strings[j]) < len(strings[min_index]):
+                min_index = j
+        strings[i], strings[min_index] = strings[min_index], strings[i]
+    return strings
 
 
 a = ["Peter", "Jon", "Andrew"]
-sort_lenght(a)
+print(sort_length(a))

e579dbc94459d1517d2d0267ca9133c1164a9357
Samuel <ogboyesam@gmail.com>
2023-07-23 11:11:55 -0700

    more update

--- Day24/calories.py
+++ Day24/calories.py
@@ -5,15 +5,16 @@ def average_calories():
     new = []
     count = 0
     print("Welcome to Average Calories Calculator")
-    days = int(input("How many days do you want to lodge: "))
+    print("Lodge in according to date, when done type 'done'")
     for i in range(1, 20):
         num = input("Day {}: ".format(i))
         if num == 'done':
             break
+
         new.append(int(num))
         count += 1
-
-    return "Your average calories intake over" + str(count) + "days is" + str(sum(new)/count)
+        average = round(sum(new)/count, 2)
+    return "Your average calories intake over " + str(count) + " days is " + str(average)
 
 
 print(average_calories())

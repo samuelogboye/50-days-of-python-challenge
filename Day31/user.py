b0287a921f953261068f6231fe623b1afe122f3c
Samuel <ogboyesam@gmail.com>
2023-08-02 16:14:39 -0700

    day31 completed

--- Day31/user.py
+++ Day31/user.py
@@ -5,4 +5,17 @@ def create_user():
     name = input("What is your name? ")
     age = int(input("How old are you? "))
     password = input("Whats your password? ")
-    
+
+    info = {'name': name, 'age': age, 'password': password}
+    print("User saved. Please login")
+    while True:
+        a_name = input("Re-enter your name: ")
+        pass_now = input("Re-enter your password: ")
+        if (a_name == info.get('name')) and (pass_now == info.get('password')):
+            print("Logged in successfully")
+            break
+        else:
+            print("Wrong password or username. Please try again")
+
+
+create_user()

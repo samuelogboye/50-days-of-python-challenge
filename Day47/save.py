6f13d01d3fc414d2a3955c8748708b5150a6b3b8
Samuel <ogboyesam@gmail.com>
2023-08-30 16:19:59 -0700

    Day47 updated

--- Day47/save.py
+++ Day47/save.py
@@ -5,8 +5,15 @@ import json
 
 def save_json():
     names = {'name': 'Carol', 'sex': 'female', 'age': 55}
-    with open('name.json', 'w', newline="") as file:
+    with open('name.json', 'w') as file:
         json.dump(names, file)
 
 
+def read_json():
+    with open('name.json', 'r') as file:
+        data = json.load(file)
+        print(data)
+
+
 save_json()
+read_json()
